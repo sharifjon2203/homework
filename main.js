6193f0b6a10fe30ed6f2e983850cf2b038a6552b
khamidullo <khkhamidullo@gmail.com>
2025-03-03 16:47:47 +0500

    dom

--- main.js
+++ main.js
@@ -1,79 +1,45 @@
-// class A {}
+const fromElement = document.querySelector("#search-form");
+const todoFormElement = document.querySelector("#todo-form");
+const todoLists = document.querySelector("#todo-list");
+let todoInputElement = document.querySelector("#todo-input");
+let searchInputElement = document.querySelector("#search-input");
 
-// class B extends A {}
+const searchSesultList = document.querySelector("#search-result");
+const todoList = [
+	{
+		name: "ali",
+		region: "uz",
+		religin: "muslim",
+	},
+];
 
-// class C extends B {}
+fromElement.addEventListener("submit", function a(e) {
+	e.preventDefault();
 
-// console.log(new B() instanceof C);
+	const value = searchInputElement.value;
 
-// console.log([1] instanceof Array);
+	const data = todoList.filter((todo) => todo === value);
 
-// const str = "asasamsdma";
-// const str2 = new String("asasamsdma");
+	for (const item of data) {
+		const liElement = document.createElement("li");
+		liElement.textContent = item;
+		searchSesultList.append(liElement);
+	}
 
-// console.log(typeof str2);
-// console.log(str2 instanceof String);
+	// const data = {};
+	// const fromMap = new FormData(this);
+	// for (const [key, value] of fromMap) {
+	// 	data[key] = value;
+	// }
+});
 
-// console.log({} instanceof Object);
+todoFormElement.addEventListener("submit", function a(e) {
+	e.preventDefault();
 
-// const a = 12;
-// a.toLowerCase();
+	const liElement = document.createElement("li");
+	liElement.textContent = todoInputElement.value;
+	todoLists.appendChild(liElement);
+	todoList.push(todoInputElement.value);
 
-// const arr = new Array(
-//   1000000000000000000000000000000000000000000000000000000000,
-// );
-
-// const url = new URL(
-//   "https://www.google.com/search?q=toshkent+metro&sca_esv=8a26c899bd46e931&source=hp&ei=2o3BZ4nXEriUxc8PwPSwiQs&iflsig=ACkRmUkAAAAAZ8Gb6kntsaD5LmVCkGFfmemu-a0ZPLrg&oq=toshkent&gs_lp=Egdnd3Mtd2l6Igh0b3Noa2VudCoCCAAyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAuGIAEMgUQLhiABDIFEAAYgAQyBRAuGIAEMgUQABiABDIFEC4YgAQyBRAAGIAESKZCUM0IWO47cAh4AJABAJgByAGgAYQQqgEGMC4xMS4xuAEDyAEA-AEBmAIQoALdEKgCAMICCxAuGIAEGNEDGMcBwgILEC4YgAQYxwEYrwHCAgcQABiABBgKwgIHEC4YgAQYCsICDRAuGIAEGMcBGAoYrwHCAgYQABgKGB7CAgUQABjvBcICCBAAGIAEGKIEmAMB8QVmFXDSk8B1IJIHBjQuMTEuMaAHkpgB&sclient=gws-wiz",
-// );
-// console.log(url);
-//
-//2
-
-/*
-const result = setTimeout(
-  (a) => {
-    console.log(a);
-  },
-  10000,
-  "HELLO",
-);
-
-const idx = setInterval(() => {
-  console.log(new Date());
-}, 1000);
-
-setTimeout(() => {
-  clearTimeout(result);
-  clearInterval(idx);
-}, 5000);
-
-*/
-
-// throw new Error("XATOLIK");
-//
-//
-// class MyError extends Error {
-//   constructor(message) {
-//     super(message);
-//   }
-// }
-// console.log(new MyError("XATO") instanceof Error);
-
-// throw new Error("XATOLIK");
-
-// try {
-//   // process.exit(1);
-//   // throw new Error("XATOLIK");
-// } catch (e) {
-//   console.log(e.stack); //message , code, stack
-// } finally {
-//   console.log("FINNALLY");
-// }
-
-process.stdout.write("salom\n");
-
-process.stdin.on("data", (data) => {
-  console.log(`You typed ${data.toString()}`);
-  process.exit();
+	todoInputElement.value = "";
 });
