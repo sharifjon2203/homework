c47c918204d83194cd099eb9d6382e3bebf50399
khamidullo <khkhamidullo@gmail.com>
2025-03-19 16:35:21 +0500

    express product CRUD

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 import express from "express";
-
+import { v4 } from "uuid";
 // const application = express();
 const app = express();
 
 const PORT = 4000;
 
-const products = [
-  { id: 1, name: "Product 1", price: 100 },
-  { id: 2, name: "Product 2", price: 200 },
-  { id: 3, name: "Product 3", price: 300 },
-  { id: 4, name: "Product 4", price: 400 },
-];
+const products = [];
 
-// http://localhost:3000 -> GET
-// req = > request
-// res => response
+// use -> middleware GET, POST, PUT, DELETE
+app.use(express.json());
+
+//custom middleware
+app.use((req, res, next) => {
+  const start = Date.now();
+  next();
+
+  const end = Date.now();
+
+  console.log(`Request took ${end - start}ms`);
+});
 
 app.get("/", (request, response) => {
   const { path, params, url, method, headers } = request;
@@ -29,28 +33,118 @@ app.get("/", (request, response) => {
   response.send("Hello from Express.js");
 });
 
-// http://localhost:3000/products -> GET -> JSON
-// Map -> set routes
-// `GET:/products`  ->  (req, res)=> {}
-
-// get all products
+//get all
 app.get("/products", (req, res) => {
   res.json(products);
 });
 
-// get product by id
-// http://localhost:3000/products/1 -> GET -> JSON
-// http://localhost:3000/products/2 -> GET -> JSON
+//get one
 app.get("/products/:id", (req, res) => {
-  const params = req.params;
-  console.log({ params });
+  try {
+    const { id } = req.params;
+    const product = products.find((product) => product.id === id);
 
-  const productId = parseInt(params.id);
-  const product = products.find((product) => product.id === productId);
+    if (!product) {
+      res.status(404).send("Product not found");
+      return;
+    }
+    res.json(product);
+  } catch (error) {
+    next(error);
+  }
+});
 
-  res.json(product);
+// create
+app.post("/products", (req, res) => {
+  try {
+    const product = req.body;
+
+    // throw new Error("Something went wrong");
+
+    // validatetion
+    if (!product || !product.name || !product.price) {
+      res.status(400).send("Name and price are required");
+      return;
+    }
+
+    product.id = v4();
+    products.push(product);
+
+    res.json(product);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+app.put("/products/:id", (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const productIndex = products.findIndex((product) => product.id === id);
+    throw new Error("Something went wrong");
+    if (productIndex < 0) {
+      res.status(404).send("Product not found");
+      return;
+    }
+
+    const newProduct = req.body;
+
+    const data = {
+      ...products[productIndex],
+      ...newProduct,
+    };
+
+    products.splice(productIndex, 1, data);
+
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+// error handling middleware
+
+app.use((error, req, res, next) => {
+  res.status(500).send(error.message);
 });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+// const a = {
+//   name: "product",
+//   price: 100,
+// };
+
+// const b = {
+//   name: "new Name",
+// };
+
+// const c = {
+//   ...a,
+//   ...b,
+// };
+
+// console.log(c);
+/*
+app.post("/register")
+1. register -> method POST > http://localhost:4000/register
+    body -> {
+        email,
+        password,
+        confirmPassword,
+        name,
+        birthday,
+        gender,
+        phone,
+    }
+app.post("/login")
+
+2. login -> method POST > http://localhost:4000/login
+    body -> {
+        email,
+        password,
+    }
+*/
